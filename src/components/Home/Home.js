a3999f5615a7b10c11287395d766dbfaefaf3132
Ryan White <mrwhiterk@gmail.com>
2019-03-19 19:31:44 -0400

    added font and styling to Homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import HomePhotos from '../HomePhotos/HomePhotos';
+import { Link } from 'react-router-dom';
+
+import './Home.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div className='row'>
-        <div className='col-6'>
-          <h3>Welcome to NBA Roster Manager</h3>
+        <div class='card w-75 offset-1'>
+          <div class='card-body'>
+            <h4 class='card-title'>Welcome to NBA Roster Manager</h4>
+            <p class='card-text'>
+              Create and manage all your favorite NBA fantasy teams
+            </p>
+            <p className='card-text'>
+              View stats, and other info from all your favorite athletes
+            </p>
+            <div className='quick-actions'>
+              <Link to='/players'>
+                <button class='btn btn-danger'>Start Adding Players</button>
+              </Link>
+              <Link to='/my-players'>
+                <button class='btn btn-danger'>View Your Players</button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
