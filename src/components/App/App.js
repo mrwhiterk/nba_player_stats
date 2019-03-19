bb984359f4c317e8f75523b1bb20c6791e5e550c
Brian Curran <curranbriant@gmail.com>
2019-03-19 17:04:06 -0400

    Add navbar and links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
-import Players from '../Players/Players';
-import Header from '../Header/Header';
-import HomePhotos from '../HomePhotos/HomePhotos';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import Players from "../Players/Players";
+import Header from "../Header/Header";
+import HomePhotos from "../HomePhotos/HomePhotos";
 
 class App extends Component {
   constructor(props) {
@@ -11,9 +12,51 @@ class App extends Component {
 
   render() {
     return (
-      <div className='container-fluid'>
-        <Header />
-        <div className='row'>
+      <div className="container-fluid">
+        <nav class="navbar navbar-expand-lg">
+          <a className="navbar-brand" href="/">
+            NBA Roster Manager
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link">
+                  <Link to="/new">
+                    <h3 className="home">Browse</h3>
+                  </Link>
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">
+                  <Link to="/new">
+                    <h3 className="home">MyTeams</h3>
+                  </Link>
+                </a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link">
+                  <Link to="/new">
+                    <h3 className="home">MyPlayers</h3>
+                  </Link>
+                  <span class="sr-only" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="row">
           <HomePhotos />
           <Players />
         </div>
