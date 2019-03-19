c53fbdbf129284dc239aff8493b62448e73d0087
Ryan White <mrwhiterk@gmail.com>
2019-03-19 18:03:36 -0400

    add home page and put players in routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,60 @@
-
-import React, { Component } from "react";
-import "./App.css";
-import { Route, Link, Switch } from "react-router-dom";
-import Players from "../Players/Players";
-import HomePhotos from "../HomePhotos/HomePhotos";
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Players from '../Players/Players';
+import HomePhotos from '../HomePhotos/HomePhotos';
 import TeamList from '../TeamList/TeamList';
-
+import Home from '../Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <nav class="navbar navbar-expand-lg">
-          <a className="navbar-brand" href="/">
-            NBA Roster Manager
-          </a>
+      <div className='container-fluid'>
+        <nav class='navbar navbar-expand-lg'>
+          <Link to='/'>NBA Roster Manager</Link>
+
           <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon" />
+            class='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'>
+            <span class='navbar-toggler-icon' />
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link">
-                  <Link to="/new">
-                    <h3 className="home">Browse</h3>
+          <div class='collapse navbar-collapse' id='navbarNav'>
+            <ul class='navbar-nav'>
+              <li class='nav-item active'>
+                <a class='nav-link'>
+                  <Link to='/players'>
+                    <h3 className='home'>Browse</h3>
                   </Link>
-                  <span class="sr-only">(current)</span>
+                  <span class='sr-only'>(current)</span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <Link to="/new">
-                    <h3 className="home">MyTeams</h3>
+              <li class='nav-item'>
+                <a class='nav-link'>
+                  <Link to='/new'>
+                    <h3 className='home'>MyTeams</h3>
                   </Link>
                 </a>
               </li>
-              <li class="nav-item active">
-                <a class="nav-link">
-                  <Link to="/new">
-                    <h3 className="home">MyPlayers</h3>
+              <li class='nav-item active'>
+                <a class='nav-link'>
+                  <Link to='/new'>
+                    <h3 className='home'>MyPlayers</h3>
                   </Link>
-                  <span class="sr-only" />
+                  <span class='sr-only' />
                 </a>
               </li>
             </ul>
           </div>
         </nav>
-        <div className="row">
-          <HomePhotos />
-          <Players />
-        </div>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/players' render={() => <Players />} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomePhotos from '../HomePhotos/HomePhotos';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='row'>
+        <div className='col-6'>
+          <h3>Welcome to NBA Roster Manager</h3>
+        </div>
+      </div>
+    );
+  }
+}
