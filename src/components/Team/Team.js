918d62297ef5074c45b6a6d0a64babc801edbdf4
Ryan White <mrwhiterk@gmail.com>
2019-03-20 18:06:13 -0400

    removed timestamp from team show page

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import serverUrl from "../constants";
-import axios from "axios";
+import React, { Component } from 'react';
+import serverUrl from '../constants';
+import axios from 'axios';
 
-import "./Team.css";
-import { Link } from "react-router-dom";
+import './Team.css';
+import { Link } from 'react-router-dom';
 
 export default class Team extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ export default class Team extends Component {
 
   getTeam() {
     axios
-      .get(serverUrl + "/teams/" + this.props.match.params.teamId)
+      .get(serverUrl + '/teams/' + this.props.match.params.teamId)
       .then(res => {
         this.setState({ team: res.data });
       })
@@ -33,9 +33,9 @@ export default class Team extends Component {
 
   deleteTeam() {
     axios
-      .delete(serverUrl + "/teams/" + this.props.match.params.teamId)
+      .delete(serverUrl + '/teams/' + this.props.match.params.teamId)
       .then(res => {
-        this.props.history.push("/my-teams");
+        this.props.history.push('/my-teams');
       })
       .catch(err => {
         console.log(err);
@@ -43,28 +43,24 @@ export default class Team extends Component {
   }
 
   render() {
-    const date = new Date(this.state.team.createdAt);
     return (
-      <div className="row">
-        <div className="card w-75 offset-1">
-          <div className="card-body">
-            <div class="row team-show-header">
-              <h1 className="card-title">{this.state.team.fullName}</h1>
-              <h4>{date && "Created:" + date.toDateString()}</h4>
-            </div>
+      <div className='row'>
+        <div className='card w-75 offset-1'>
+          <div className='card-body'>
+            <h1 className='card-title'>{this.state.team.fullName}</h1>
             <Link to={`/editTeam/${this.state.team._id}`}>
-              <button class="btn btn-danger">Edit</button>
+              <button class='btn btn-danger'>Edit</button>
             </Link>
 
-            <button onClick={this.deleteTeam} class="btn btn-danger">
+            <button onClick={this.deleteTeam} class='btn btn-danger'>
               Delete
             </button>
 
-            <h3 className="card-title">Roster</h3>
-            <ul className="card-text">
+            <h3 className='card-title'>Roster</h3>
+            <ul className='card-text'>
               {this.state.team.teamRoster &&
                 this.state.team.teamRoster.map((player, i) => (
-                  <li key={i} class="rosterItem">
+                  <li key={i} class='rosterItem'>
                     <h5>
                       {player.lastName}, {player.firstName}
                     </h5>
