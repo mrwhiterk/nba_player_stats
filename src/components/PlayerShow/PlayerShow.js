a708c78de16ded00c4b0aeeb192a8094ed3998e7
Brian Curran <curranbriant@gmail.com>
2019-03-20 20:08:04 -0400

    added player photo

--- src/components/PlayerShow/PlayerShow.js
+++ src/components/PlayerShow/PlayerShow.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import serverUrl from "../constants";
-import axios from "axios";
-import { league } from "../../players.json";
-import "./PlayerShow.css";
+import React, { Component } from 'react';
+import serverUrl from '../constants';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+import { league } from '../../players.json';
+import './PlayerShow.css';
 
 const players = league.standard;
 
@@ -10,7 +11,7 @@ class PlayerShow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      player: []
+      player: [],
     };
   }
 
@@ -18,33 +19,45 @@ class PlayerShow extends Component {
     this.setState({
       player: players.find(
         player => player.personId === this.props.match.params.personId
-      )
+      ),
     });
   }
 
   render() {
+    const image = this.props.photos;
     return (
-      <div className="row">
-        <div className="card w-75 offset</div>-1">
-          <div className="card-body">
-            <div className="player-title-show">
-              <h1 className="card-title">
-                {this.state.player.firstName + " " + this.state.player.lastName}
-                <h1> {this.state.player.jersey} </h1>
+      <div className='row'>
+        <div className='card w-75 offset</div>-1'>
+          <div />
+          <div className='card-body'>
+            <div className='player-title-show'>
+              <h1 className='card-title'>
+                {this.state.player.firstName + ' ' + this.state.player.lastName}
               </h1>
             </div>
-            <div className="content">
-              <h2>Position: {this.state.player.pos}</h2>
-              <h2>
-                Height:{" "}
-                {this.state.player.heightFeet +
-                  "'" +
-                  this.state.player.heightInches +
-                  '"'}
-              </h2>
-              <h2>Weight: {this.state.player.weightPounds}</h2>
-              <h2>Years Pro: {this.state.player.yearsPro}</h2>
-              <h2>College: {this.state.player.collegeName}</h2>
+            <div className='row'>
+              <div className='col-6'>
+                <h2>Jersey Number: {this.state.player.jersey} </h2>
+                <h2>Position: {this.state.player.pos}</h2>
+                <h2>
+                  Height:{' '}
+                  {this.state.player.heightFeet +
+                    "'" +
+                    this.state.player.heightInches +
+                    '"'}
+                </h2>
+                <h2>Weight: {this.state.player.weightPounds}</h2>
+                <h2>Years Pro: {this.state.player.yearsPro}</h2>
+                <h2>College: {this.state.player.collegeName}</h2>
+              </div>
+              <div className='col-6'>
+                <img
+                  className='playerPhoto'
+                  src={`https://nba-players.herokuapp.com/players/${
+                    this.state.player.lastName
+                  }/${this.state.player.firstName}`}
+                />
+              </div>
             </div>
           </div>
         </div>
