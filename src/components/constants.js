2865391715def2827135a140703264e77876251c
Ryan White <mrwhiterk@gmail.com>
2019-03-22 16:21:25 -0400

    changed server to https

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,5 @@
 const serverUrl = window.location.host.includes('localhost')
   ? 'http://localhost:3001'
-  : 'http://afternoon-mesa-92635.herokuapp.com';
+  : 'https://afternoon-mesa-92635.herokuapp.com';
 
 export default serverUrl;
